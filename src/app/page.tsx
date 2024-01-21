7e881fba3d19a8e5e04584d5cfe8a730cb6ca264
Filipe Braga <filipe.b.braga@gmail.com>
2024-01-20 22:55:01 -0300

    fix: using next 13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Hall from '@/components/Hall';
 import React, { useState, useEffect } from 'react';
+import Hall from '../components/Hall';
 
 export default function Home() {
     return <Hall />;
